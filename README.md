f34009e07f8948f469b6541199d9bf718492bda3
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-02-02 11:22:36 -0500

    init

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ https://github.com/IBM-Cloud/terraform-provider-ibm/tree/master/examples
 https://cloud.ibm.com/docs/terraform?topic=terraform-manage_resources
 
 https://ibm-cloud.github.io/tf-ibm-docs/
+
+
+* https://cloud.ibm.com/docs/terraform?topic=terraform-setup_cli#store_credentials
+
+* https://www.ibm.com/cloud/blog/automate-kubernetes-infrastructure-with-ibm-cloud-schematics
--- sample.tf
+++ sample.tf
@@ -1,16 +1,22 @@
 
-resource "ibm_resource_group" "group" {
-  name = "a-resource-grp"
+resource "ibm_resource_group" "my_grp" {
+  name = "my-resource-grp"
 }
 
-data "ibm_resource_group" "group" {
-  name = "a-resource-grp"
+resource "ibm_resource_instance" "cloudant_instance" {
+  name              = "test-cloudant-tst"
+  service           = "cloudantnosqldb"
+  plan              = "lite"
+  location          = "us-south"
+  resource_group_id = ibm_resource_group.my_grp.id
 }
 
-resource "ibm_resource_instance" "resource_instance" {
-  name       = "test-cloudant-tst"
-  service    = "cloudantnosqldb"
-  plan       = "lite"
-  location = "us-south"
-  resource_group_id = data.ibm_resource_group.group.id
+# The Kubernetes Cluster Resource
+resource "ibm_container_cluster" "mycluster-terraform" {
+  name              = "mycluster-terraform"
+  datacenter        = "dal10"
+  machine_type      = "free"
+  hardware          = "shared"
+  default_pool_size = 1
+  resource_group_id = ibm_resource_group.my_grp.id
 }
