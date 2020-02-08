6bb0e7b0d8f08da7732594fe46173863c14b3253
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-02-08 16:38:19 -0500

    init

--- README.md
+++ README.md
@@ -11,3 +11,30 @@ https://ibm-cloud.github.io/tf-ibm-docs/
 * https://cloud.ibm.com/docs/terraform?topic=terraform-setup_cli#store_credentials
 
 * https://www.ibm.com/cloud/blog/automate-kubernetes-infrastructure-with-ibm-cloud-schematics
+
+
+https://github.com/IBM-Cloud/terraform-provider-ibm/issues/804
+
+
+
+557  bx catalog
+  558  bx catalog service
+  559  bx catalog service-marketplace
+  560  bx catalog service-marketplace | grep -i sysdig
+  561  bx catalog service-marketplace | grep -i cloudant
+  562  bx catalog service-marketplace shjks
+  563  clear
+  564  bx catalog service-marketplace | grep -i cloudant
+  565  bx catalog service-marketplace | grep -i sysdig
+  566  bx catalog service-marketplace | grep -i cloudant | grep lite
+  567  bx catalog service-marketplace | grep -i sysdig | grep trial
+  568  bx catalog service-marketplace | grep -i sysdig 
+  569  bx catalog service cloudantnosqldb
+  570  bx catalog service sysdig-monitor 
+
+
+
+   535  terraform init
+  536  terraform validate
+  537  terraform plan
+  538  terraform apply
--- sample.tf
+++ sample.tf
@@ -1,22 +1,52 @@
-
-resource "ibm_resource_group" "my_grp" {
-  name = "my-resource-grp"
+resource "ibm_resource_group" "olivieri_grp" {
+  name = "olivieri_group"
 }
 
-resource "ibm_resource_instance" "cloudant_instance" {
-  name              = "test-cloudant-tst"
-  service           = "cloudantnosqldb"
-  plan              = "lite"
+resource "ibm_resource_instance" "olivieri-sysdig" {
+  name              = "olivieri-sysdig"
+  service           = "sysdig-monitor"
+  plan              = "graduated-tier"
   location          = "us-south"
-  resource_group_id = ibm_resource_group.my_grp.id
+  resource_group_id = ibm_resource_group.olivieri_grp.id
 }
 
 # The Kubernetes Cluster Resource
-resource "ibm_container_cluster" "mycluster-terraform" {
-  name              = "mycluster-terraform"
+#resource "ibm_container_cluster" "olivieri-mycluster-terraform" {
+#  name              = "mycluster-terraform"
+#  datacenter        = "dal10"
+#  machine_type      = "free"
+#  hardware          = "shared"
+#  default_pool_size = 1
+#  resource_group_id = ibm_resource_group.olivieri_grp.id
+#}
+
+#resource "ibm_network_vlan" "public_vlan" {
+#  name            = "public_vlan1"
+#  datacenter      = "dal10"
+#  type            = "PUBLIC"
+#}
+
+resource "ibm_container_cluster" "olivieri_cluster" {
+  name              = "olivieri_cluster"
   datacenter        = "dal10"
-  machine_type      = "free"
-  hardware          = "shared"
+  machine_type      = "b3c.4x16"
+  hardware          = "dedicated"
   default_pool_size = 1
-  resource_group_id = ibm_resource_group.my_grp.id
+  public_vlan_id    = 2779752
+  private_vlan_id   = 2779754
+  resource_group_id = ibm_resource_group.olivieri_grp.id
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "zone2" {
+  cluster     = ibm_container_cluster.olivieri_cluster.id
+  worker_pool = "default"
+  zone        = "dal12"
+  resource_group_id = ibm_resource_group.olivieri_grp.id
+}
+
+resource "ibm_container_worker_pool_zone_attachment" "zone3" {
+  cluster     = ibm_container_cluster.olivieri_cluster.id
+  worker_pool = "default"
+  zone        = "dal13"
+  resource_group_id = ibm_resource_group.olivieri_grp.id
 }
